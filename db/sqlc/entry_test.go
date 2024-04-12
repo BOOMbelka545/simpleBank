6cfebab4d5fba62f3adaff022b6d3e237fd7fcc6
Леонид Замореев <PWDIT@MacBook-Pro-Oleg.local>
2024-04-12 22:07:24 +0300

    test for entry

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,102 @@
+package db
+
+import (
+	"context"
+	"simpleBank/util"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, error := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, error)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+}
+
+func createRandomEntry(t *testing.T) Entry {
+	account := createRandomAccount(t)
+
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, error := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, error)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+
+	entry2, error := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, error)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt.Time, entry2.CreatedAt.Time, time.Second)
+}
+
+func createEntryByID(t *testing.T, accountID int64) {
+	arg := CreateEntryParams{
+		AccountID: accountID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, error := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, error)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createEntryByID(t, account.ID)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, error := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, error)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
